4b253bcd06e7e010848807e4af0b3e2faf8a12e3
Camilo sanchez <regimensur01@gmail.com>
2019-09-08 22:03:29 -0400

    proyecto con autenticación
    Autenticación

--- konradtaller/config/routes.rb
+++ konradtaller/config/routes.rb
@@ -1,8 +1,26 @@
-Rails.application.routes.draw do
+ # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  Rails.application.routes.draw do
+  root 'home#index'
+  
+  resources :users
+  resources :sessions, only: [:new, :create, :destroy]
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  
+
+  
+  get 'sessions/new'
+  get 'sessions/create'
+  get 'sessions/destroy' 
   resources :appoiments
   resources :consulting_rooms
   resources :doctors
   resources :professions
   resources :patients
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :users
+  
+ 
+  
+
 end
--- konradtaller/db/schema.rb
+++ konradtaller/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_05_165809) do
+ActiveRecord::Schema.define(version: 2019_09_09_014220) do
 
   create_table "appoiments", force: :cascade do |t|
     t.integer "consulting_room_id", null: false
@@ -58,6 +58,14 @@ ActiveRecord::Schema.define(version: 2019_09_05_165809) do
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email"
+    t.string "password_digest"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "appoiments", "consulting_rooms"
